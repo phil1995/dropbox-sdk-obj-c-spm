54fa227b153706c84d328535ae566a3a6cce2528
runner <runner@Mac-1709918771080.local>
2024-03-08 17:34:07 +0000

    Version and checksum update for 7.1.0

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
             targets: ["ObjectiveDropboxOfficial"]),
     ],
     targets: [
-        .binaryTarget(name: "ObjectiveDropboxOfficial", url: "https://github.com/phil1995/dropbox-sdk-obj-c/releases/download/7.0.0-spm/ObjectiveDropboxOfficial.xcframework.zip", checksum: "edaaf9621390224e326b68b87f6f78a31bc3418ca816a778d765f7f4012a5b11") 
+        .binaryTarget(name: "ObjectiveDropboxOfficial", url: "https://github.com/phil1995/dropbox-sdk-obj-c/releases/download/7.1.0-spm/ObjectiveDropboxOfficial.xcframework.zip", checksum: "64eb76cc7bea35251a010c80e2e2e065f4d28d9cf4a773bb3f7712b9cde862c8") 
     ]
 )
